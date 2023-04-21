4889183888f9a92d8dfbe72228a2c36d23a15fdf
Rogerio Vieira <rogeriovbh@gmail.com>
2023-04-21 20:59:56 -0300

    adiciona css de escopo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import styles from '../styles/home.module.css'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <h1 className={styles.title}>Hello world</h1>
   )
 }
