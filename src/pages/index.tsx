f53d9820ee3a75952f1eff595d296003cb5b6b5d
Rogerio Vieira <rogeriovbh@gmail.com>
2023-04-22 00:42:42 -0300

    inicia estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,13 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '../styles/home.module.scss'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <Head>
-          <title>Inicio | ig.news</title>
-        </Head>
+        <title>Inicio | ig.news</title>
+      </Head>
       <h1 className={styles.title}>Hello world</h1>
     </>
 
